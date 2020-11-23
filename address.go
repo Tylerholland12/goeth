6abb6491a8a035554b2d913d93dadec01ab4aafd
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-11-22 18:36:44 -0600

    [update] address changed to ganache

--- address.go
+++ address.go
@@ -7,11 +7,11 @@ import (
 )
 
 func main() {
-	address := common.HexToAddress("0x71c7656ec7ab88b098defb751b7401b5f6d8976f")
+	address := common.HexToAddress("0xbeE0900C04159CBa4e154968047914487974d3f0")
 
 	fmt.Println(address.Hex()) // 0x71C7656EC7ab88b098defB751B7401B5f6d8976F
 
-	fmt.Println(address.Hash().Hex()) // 0x00000000000000000000000071c7656ec7ab88b098defb751b7401b5f6d8976f
+	fmt.Println(address.Hash().Hex()) // 0x000000000000000000000000bee0900c04159cba4e154968047914487974d3f0
 
-	fmt.Println(address.Bytes())      // [113 199 101 110 199 171 136 176 152 222 251 117 27 116 1 181 246 216 151 111]
+	fmt.Println(address.Bytes())      // [190 224 144 12 4 21 156 186 78 21 73 104 4 121 20 72 121 116 211 240]
 }
