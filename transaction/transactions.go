0812be435145cdd017b6c67074a3270dfe7372ee
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-11-23 15:59:07 -0600

    [update] transactions not yet working

--- transaction/transactions.go
+++ transaction/transactions.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-    client, err := ethclient.Dial("https://mainnet.infura.io")
+    client, err := ethclient.Dial("http://localhost:8545")
     if err != nil {
         log.Fatal(err)
     }
