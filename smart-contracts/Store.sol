4c794d4ca6b8863c061445c9cf83e8bcc71a10a7
Tyler_Holland <tyler.holland@students.makeschool.com>
2020-11-27 08:30:13 -0600

    [update]

--- smart-contracts/Store.sol
+++ smart-contracts/Store.sol
@@ -1,17 +1,17 @@
 pragma solidity ^0.4.24;
 
 contract Store {
-    event ItemSet(bytes32 key, bytes32 value);
+  event ItemSet(bytes32 key, bytes32 value);
 
-    string public version;
-    mapping (bytes32 => bytes32) public items;
+  string public version;
+  mapping (bytes32 => bytes32) public items;
 
-    constructor(string _version) public {
-        version = _version;
-    }
+  constructor(string _version) public {
+    version = _version;
+  }
 
-    function setItem(bytes32 key, bytes32 value) external {
-        items[key] = value;
-        emit ItemSet(key, value);
-    }
+  function setItem(bytes32 key, bytes32 value) external {
+    items[key] = value;
+    emit ItemSet(key, value);
+  }
 }
